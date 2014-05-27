c760e65a8b70049e2c36ca4656d05ccf5b636190
Conall <ocallaghan@gmail.com>
2014-05-27 17:33:50 -0400

    added a window and box to display

--- tests/test_server.lua
+++ tests/test_server.lua
@@ -13,6 +13,15 @@ thnode: a Torch compute node
    -p,--print dont use ncurses
 ]])
 
+local function create_newwin(height, width, starty, startx)
+   local local_win;
+
+	local_win = curses.newwin(height, width, starty, startx)
+	curses.box(local_win, 0 , 0)		
+   curses.wrefresh(local_win)
+	return local_win;
+end
+
 
 
 local status_callback 
@@ -49,22 +58,21 @@ local node_names = {}
 local node_area = {}
 
 local updateNode = function(nodename, workername)
-   local box = node_area[nodename]
+   local win = node_area[nodename]
    local nodeEntry = nodes[nodename]
-   if opt.print then
-      print(box)
-      print(nodeEntry)
-      return 
-   end
-   curses.mvprintw(box.startx, box.starty, nodename)
-   curses.mvprintw(box.startx + 1, box.starty, "Number of workers: " .. #nodeEntry.worker_names)
-   curses.mvprintw(box.startx + 2, box.starty, "Last Seen: " .. nodeEntry.last_seen)
+
+   curses.mvwprintw(win, 1, 1, "%s", nodename)
+   curses.mvwprintw(win, 2, 1, "Number of workers: %d", #nodeEntry.worker_names)
+   curses.mvwprintw(win, 3, 1, "Last Seen: %d", nodeEntry.last_seen)
 
    for i,workername in ipairs(nodeEntry.worker_names) do
-      curses.mvprintw(box.startx + 2 + i, box.starty, "Worker: " .. workername .. " " .. tostring(pretty.write(nodeEntry.workers[workername].status or "")))   
+      curses.mvwprintw(win, 3 + i, 1, "Worker: %s", workername)   
    end
 
+   curses.box(win, 0 , 0)	
+   curses.wrefresh(win)
    curses.refresh()
+
 
 end
 
@@ -72,10 +80,10 @@ local onNewNode = function(name)
    table.insert(node_names,name)
    nodes[name] = {workers = {}, last_seen = os.time(), worker_names = {}}
    
-   local numnodes = #node_names
+   local numnodes = #node_names - 1
    local startx = (numnodes * 10) % 60
    local starty = math.floor(numnodes / 40) * 25
-   node_area[name] = {startx = startx, starty = starty}
+   node_area[name] = create_newwin(25, 60, startx, starty)
 end
 
 local onNewWorker = function(nodename, workername)
